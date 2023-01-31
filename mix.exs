db9d34320d00a4ab87ab3e07dbc0c1a1e3c3303b
Arthur Colle <hello@arthurcolle.com>
2023-01-30 22:39:31 -0500

    Emergency bugfix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DeribitApi.MixProject do
   def project do
     [
       app: :deribit_api,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
